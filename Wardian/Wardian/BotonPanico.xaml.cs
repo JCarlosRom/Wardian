8de837be4e71640cb4b6340792c4db98af69edf0
JCarlosRom <jcarlosrom97@hotmail.com>
2019-03-19 16:50:57 -0600

    Iniciación de proyecto

---
+++ Wardian/Wardian/BotonPanico.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wardian
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BotonPanico : ContentPage
+	{
+		public BotonPanico ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void btnPanico_click(object sender, EventArgs e)
+        {
+            StartAnimation();
+        }
+
+        private async void StartAnimation()
+        {
+            
+            await Task.Delay(200);
+            await btnPanico.FadeTo(0, 250);
+            await Task.Delay(200);
+            await btnPanico.FadeTo(1, 250);
+        }
+    }
+}
---
+++ Wardian/Wardian/Login.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Wardian
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnMenu(object sender, EventArgs e)
+        {
+            if (Navigation != null)
+            {
+
+                var psl = new Menu();
+                await Navigation.PushAsync(psl);
+            }
+        }
+    }
+}
